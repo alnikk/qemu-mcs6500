76903e91fbb9a698b024a56cc291594c87468a2c
Alexandre Guyon <aguyon@mail.corp.sentryo.net>
2022-01-10 17:19:48 +0100

    target/mos65xx: add some cpu parameters

---
+++ target/mos65xx/cpu.h
@@ -0,0 +1,26 @@
+/*
+ *  MOS65xx emulation
+ *
+ *  Copyright (c) 2022 Alexandre Guyon
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MOS65XX_CPU_H
+#define MOS65XX_CPU_H
+
+/* We don't support user mode */
+#define TCG_GUEST_DEFAULT_MO 0
+
+#endif // MOS65XX_CPU_H
